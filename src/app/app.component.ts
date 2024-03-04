633f34560b61ed6e3fbb9e8ada80aff778a9d3ef
abhay <abhay.nepal.9@gmail.com>
2024-03-04 23:24:36 +0545

    added template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'v4lo-front';
+  constructor(private titleService:Title,private modalsService:NgbModal){
+    titleService.setTitle("V4L0");
+  }
+  public open(modal:any):void{
+    this.modalsService.open(modal);
+  }
 }
