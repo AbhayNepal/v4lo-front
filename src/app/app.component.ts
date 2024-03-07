6fd9414c9654fb3eeccd8f269b16c3435f71d749
abhay <abhay.nepal.9@gmail.com>
2024-03-07 09:14:39 +0545

    implemented routes for home and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { RegisterComponent } from '../register/register.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NgbModule],
+  imports: [CommonModule, RouterOutlet,NgbModule,RegisterComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,6 +16,8 @@ export class AppComponent {
   constructor(private titleService:Title,private modalsService:NgbModal){
     titleService.setTitle("V4L0");
   }
+
+
   public open(modal:any):void{
     this.modalsService.open(modal);
   }
